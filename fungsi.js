6bc812d5097cabbd9bc951239dfcb3aee9b21e2b
Mukhamad Aziz Firmansyah <mukhamadazizfirmansyah@gmail.com>
2023-06-25 08:27:08 +0700

    merevisi commit perkalaian

--- fungsi.js
+++ fungsi.js
@@ -4,8 +4,8 @@ console.log(add(1, 2));
 const subtract = (a, b) => a - b;
 console.log(subtract(5, 2));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(3, 3));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(3, 3));
 
 const divie = (a, b) => a / b;
 console.log(divide(9 / 3));
